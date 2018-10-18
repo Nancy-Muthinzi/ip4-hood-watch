8276b5d102bb818821d199a99ca79f19ec273821
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-18 10:01:46 +0300

    create model profile, make migration

--- hoodwatch/models.py
+++ hoodwatch/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile', default=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    profile_pic = models.ImageField(upload_to='profile/', default=True)
+    bio = models.CharField(max_length=200, default=True)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
